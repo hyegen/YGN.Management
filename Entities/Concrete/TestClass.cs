71120f6c4ad2fb222d7c22f9b523fa8b86b71465
hyegen <hyegen205@gmail.com>
2023-12-28 14:41:06 +0300

    Test Class has been added.

---
+++ Entities/Concrete/TestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class TestClass
+    {
+        public int ID { get; set; }
+        public int DepartmentID { get; set; }
+    }
+}
